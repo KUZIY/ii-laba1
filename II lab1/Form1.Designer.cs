2dfd1396f3e53cb882ec53899ac16e5d3a184ed6
KUZIY <82047999+KUZIY@users.noreply.github.com>
2022-10-21 00:36:14 +0300

    1 com
    -

---
+++ II lab1/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace II_lab1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.RecognizeButton = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1013, 580);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // RecognizeButton
+            // 
+            this.RecognizeButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.RecognizeButton.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.RecognizeButton.Location = new System.Drawing.Point(869, 598);
+            this.RecognizeButton.Name = "RecognizeButton";
+            this.RecognizeButton.Size = new System.Drawing.Size(156, 49);
+            this.RecognizeButton.TabIndex = 1;
+            this.RecognizeButton.Text = "Распознать";
+            this.RecognizeButton.UseVisualStyleBackColor = true;
+            this.RecognizeButton.Click += new System.EventHandler(this.RecognizeButton_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "JPG|*.jpg|PNG|*.png";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1037, 659);
+            this.Controls.Add(this.RecognizeButton);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Face";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button RecognizeButton;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ II lab1/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+namespace II_lab1
+{
+    public partial class Form1 : Form
+    {
+        private static CascadeClassifier classifierface = new CascadeClassifier("haarcascade_frontalface_alt_tree.xml");
+        private static CascadeClassifier classifiereye = new CascadeClassifier("haarcascade_eye.xml");
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void RecognizeButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DialogResult res = openFileDialog1.ShowDialog();
+                if (res == DialogResult.OK)
+                {
+                    string path = openFileDialog1.FileName;
+                    pictureBox1.Image = Image.FromFile(path);
+                    Bitmap bitmap = new Bitmap(pictureBox1.Image);
+                    Image<Bgr, byte> grayImage = new Image<Bgr, byte>(bitmap);
+                    Rectangle[] faces = classifierface.DetectMultiScale(grayImage, 1.01,2);
+                    foreach (Rectangle face in faces)
+                    {
+                        using (Graphics graphics = Graphics.FromImage(bitmap))
+                        {
+                            using (Pen pen = new Pen(Color.Red, 3))
+                            {
+                                graphics.DrawRectangle(pen, face);
+                            }
+                        }
+                    }
+                    Rectangle[] eyes = classifiereye.DetectMultiScale(grayImage,1.08,13);
+                    foreach (Rectangle eye in eyes)
+                    {
+                        using (Graphics graphics = Graphics.FromImage(bitmap))
+                        {
+                            using (Pen pen = new Pen(Color.Yellow, 3))
+                            {
+                                graphics.DrawRectangle(pen, eye);
+                            }
+                        }
+                    }
+                    pictureBox1.Image = bitmap;
+                }   
+                else
+                {
+                    MessageBox.Show("Изображение не выбрано", "Ошибка");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка");
+            }
+        }
+    }
+}
