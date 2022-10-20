35ef368ac63437b95286af584adcf04991f6c482
KUZIY <82047999+KUZIY@users.noreply.github.com>
2022-10-21 02:47:29 +0300

    2 comit
    -

--- II lab1/Form1.Designer.cs
+++ II lab1/Form1.Designer.cs
@@ -29,57 +29,170 @@
         private void InitializeComponent()
         {
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.RecognizeButton = new System.Windows.Forms.Button();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripComboBox1 = new System.Windows.Forms.ToolStripComboBox();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.StopButton = new System.Windows.Forms.Button();
+            this.PauseButton = new System.Windows.Forms.Button();
+            this.StartButton = new System.Windows.Forms.Button();
+            this.ExitButton = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.toolStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(15, 51);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(1013, 580);
+            this.pictureBox1.Size = new System.Drawing.Size(1013, 596);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
-            // 
-            // RecognizeButton
-            // 
-            this.RecognizeButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.RecognizeButton.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.RecognizeButton.Location = new System.Drawing.Point(869, 598);
-            this.RecognizeButton.Name = "RecognizeButton";
-            this.RecognizeButton.Size = new System.Drawing.Size(156, 49);
-            this.RecognizeButton.TabIndex = 1;
-            this.RecognizeButton.Text = "Распознать";
-            this.RecognizeButton.UseVisualStyleBackColor = true;
-            this.RecognizeButton.Click += new System.EventHandler(this.RecognizeButton_Click);
+            this.pictureBox1.UseWaitCursor = true;
             // 
             // openFileDialog1
             // 
             this.openFileDialog1.FileName = "openFileDialog1";
             this.openFileDialog1.Filter = "JPG|*.jpg|PNG|*.png";
             // 
+            // toolStrip1
+            // 
+            this.toolStrip1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(50)))), ((int)(((byte)(41)))));
+            this.toolStrip1.Dock = System.Windows.Forms.DockStyle.None;
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripLabel1,
+            this.toolStripComboBox1,
+            this.toolStripSeparator1});
+            this.toolStrip1.Location = new System.Drawing.Point(9, 9);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(419, 32);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            this.toolStrip1.UseWaitCursor = true;
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(50)))), ((int)(((byte)(41)))));
+            this.toolStripLabel1.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.toolStripLabel1.ForeColor = System.Drawing.SystemColors.MenuBar;
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(66, 29);
+            this.toolStripLabel1.Text = "Камера:";
+            // 
+            // toolStripComboBox1
+            // 
+            this.toolStripComboBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(50)))), ((int)(((byte)(41)))));
+            this.toolStripComboBox1.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.toolStripComboBox1.ForeColor = System.Drawing.SystemColors.MenuBar;
+            this.toolStripComboBox1.Name = "toolStripComboBox1";
+            this.toolStripComboBox1.Size = new System.Drawing.Size(300, 32);
+            this.toolStripComboBox1.SelectedIndexChanged += new System.EventHandler(this.toolStripComboBox1_SelectedIndexChanged);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 28);
+            // 
+            // StopButton
+            // 
+            this.StopButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(50)))), ((int)(((byte)(41)))));
+            this.StopButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.StopButton.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.StopButton.ForeColor = System.Drawing.SystemColors.MenuBar;
+            this.StopButton.Location = new System.Drawing.Point(869, 9);
+            this.StopButton.Name = "StopButton";
+            this.StopButton.Size = new System.Drawing.Size(75, 36);
+            this.StopButton.TabIndex = 2;
+            this.StopButton.Text = "Стоп";
+            this.StopButton.UseVisualStyleBackColor = false;
+            this.StopButton.UseWaitCursor = true;
+            this.StopButton.Click += new System.EventHandler(this.StopButton_Click);
+            // 
+            // PauseButton
+            // 
+            this.PauseButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(50)))), ((int)(((byte)(41)))));
+            this.PauseButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.PauseButton.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.PauseButton.ForeColor = System.Drawing.SystemColors.MenuBar;
+            this.PauseButton.Location = new System.Drawing.Point(788, 9);
+            this.PauseButton.Name = "PauseButton";
+            this.PauseButton.Size = new System.Drawing.Size(75, 36);
+            this.PauseButton.TabIndex = 3;
+            this.PauseButton.Text = "Пауза";
+            this.PauseButton.UseVisualStyleBackColor = false;
+            this.PauseButton.UseWaitCursor = true;
+            this.PauseButton.Click += new System.EventHandler(this.PauseButton_Click);
+            // 
+            // StartButton
+            // 
+            this.StartButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(50)))), ((int)(((byte)(41)))));
+            this.StartButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.StartButton.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.StartButton.ForeColor = System.Drawing.SystemColors.MenuBar;
+            this.StartButton.Location = new System.Drawing.Point(707, 9);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(75, 36);
+            this.StartButton.TabIndex = 4;
+            this.StartButton.Text = "Пуск";
+            this.StartButton.UseVisualStyleBackColor = false;
+            this.StartButton.UseWaitCursor = true;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(50)))), ((int)(((byte)(41)))));
+            this.ExitButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ExitButton.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ExitButton.ForeColor = System.Drawing.SystemColors.MenuBar;
+            this.ExitButton.Location = new System.Drawing.Point(950, 9);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(75, 36);
+            this.ExitButton.TabIndex = 5;
+            this.ExitButton.Text = "Выход";
+            this.ExitButton.UseVisualStyleBackColor = false;
+            this.ExitButton.UseWaitCursor = true;
+            this.ExitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(130)))), ((int)(((byte)(106)))), ((int)(((byte)(95)))));
             this.ClientSize = new System.Drawing.Size(1037, 659);
-            this.Controls.Add(this.RecognizeButton);
+            this.Controls.Add(this.ExitButton);
+            this.Controls.Add(this.StartButton);
+            this.Controls.Add(this.PauseButton);
+            this.Controls.Add(this.StopButton);
+            this.Controls.Add(this.toolStrip1);
             this.Controls.Add(this.pictureBox1);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form1";
             this.Text = "Face";
+            this.UseWaitCursor = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Button RecognizeButton;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+        private System.Windows.Forms.ToolStripComboBox toolStripComboBox1;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.Button StopButton;
+        private System.Windows.Forms.Button PauseButton;
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.Button ExitButton;
     }
 }
 
--- II lab1/Form1.cs
+++ II lab1/Form1.cs
@@ -7,64 +7,212 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Emgu;
 using Emgu.CV;
+using Emgu.CV.Util;
 using Emgu.CV.Structure;
+using Emgu.Util;
+using DirectShowLib;
 
 namespace II_lab1
 {
     public partial class Form1 : Form
     {
+        private VideoCapture capture = null;
+        private DsDevice[] webCams = null;
+        private int selectedCameraid = 0;
         private static CascadeClassifier classifierface = new CascadeClassifier("haarcascade_frontalface_alt_tree.xml");
         private static CascadeClassifier classifiereye = new CascadeClassifier("haarcascade_eye.xml");
         public Form1()
         {
             InitializeComponent();
         }
+        private Bitmap Find(Bitmap image)
+        {
+            Bitmap bitmap = new Bitmap(image);
+            Image<Bgr, byte> grayImage = new Image<Bgr, byte>(bitmap);
 
-        private void RecognizeButton_Click(object sender, EventArgs e)
+            Rectangle[] faces = classifierface.DetectMultiScale(grayImage, 1.01, 2);
+            foreach (Rectangle face in faces)
+            {
+                using (Graphics graphics = Graphics.FromImage(bitmap))
+                {
+                    using (Pen pen = new Pen(Color.Red, 3))
+                    {
+                        graphics.DrawRectangle(pen, face);
+                    }
+                }
+            }
+
+            Rectangle[] eyes = classifiereye.DetectMultiScale(grayImage, 1.08, 13);
+            //int recCount = 9;
+            //Rectangle[] eyes = classifiereye.DetectMultiScale(grayImage, 1.08, recCount); ;
+            //while (eyes.Count() % 2 != 0 || eyes.Count() > 20)
+            //{
+            //    eyes = classifiereye.DetectMultiScale(grayImage, 1.08, recCount);
+            //    recCount++;
+            //}
+
+            foreach (Rectangle eye in eyes)
+            {
+                using (Graphics graphics = Graphics.FromImage(bitmap))
+                {
+                    using (Pen pen = new Pen(Color.Yellow, 3))
+                    {
+                        graphics.DrawRectangle(pen, eye);
+                    }
+                }
+            }
+            return bitmap;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            webCams = DsDevice.GetDevicesOfCat(FilterCategory.VideoInputDevice); 
+            for (int i =0; i < webCams.Length; i++)
+            {
+                toolStripComboBox1.Items.Add(webCams[i].Name);
+            }
+        }
+
+        private void StartButton_Click(object sender, EventArgs e)
         {
             try
             {
-                DialogResult res = openFileDialog1.ShowDialog();
-                if (res == DialogResult.OK)
-                {
-                    string path = openFileDialog1.FileName;
-                    pictureBox1.Image = Image.FromFile(path);
-                    Bitmap bitmap = new Bitmap(pictureBox1.Image);
-                    Image<Bgr, byte> grayImage = new Image<Bgr, byte>(bitmap);
-                    Rectangle[] faces = classifierface.DetectMultiScale(grayImage, 1.01,2);
-                    foreach (Rectangle face in faces)
-                    {
-                        using (Graphics graphics = Graphics.FromImage(bitmap))
-                        {
-                            using (Pen pen = new Pen(Color.Red, 3))
-                            {
-                                graphics.DrawRectangle(pen, face);
-                            }
-                        }
-                    }
-                    Rectangle[] eyes = classifiereye.DetectMultiScale(grayImage,1.08,13);
-                    foreach (Rectangle eye in eyes)
-                    {
-                        using (Graphics graphics = Graphics.FromImage(bitmap))
-                        {
-                            using (Pen pen = new Pen(Color.Yellow, 3))
-                            {
-                                graphics.DrawRectangle(pen, eye);
-                            }
-                        }
-                    }
-                    pictureBox1.Image = bitmap;
-                }   
+                if (webCams.Length == 0) throw new Exception("Нет доступных камер!");
+                else if (toolStripComboBox1.SelectedItem == null) throw new Exception("Необходимо выбрать камеру!!");
+                else if (capture != null) capture.Start();
                 else
                 {
-                    MessageBox.Show("Изображение не выбрано", "Ошибка");
+                    capture = new VideoCapture(selectedCameraid);
+                    capture.ImageGrabbed += Capture_ImageGrabbed;
+                    capture.Start();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Capture_ImageGrabbed(object sender, EventArgs e)
+        {
+            try
+            {
+                Mat m = new Mat();
+                capture.Retrieve(m);
+                Bitmap outImage = Find(m.ToImage<Bgr, byte>().Flip(Emgu.CV.CvEnum.FlipType.Horizontal).Bitmap);
+                pictureBox1.Image = outImage;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void PauseButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (capture != null)
+                {
+                    capture.Pause();
                 }
             }
             catch (Exception ex)
             {
-                MessageBox.Show(ex.Message, "Ошибка");
+                MessageBox.Show(ex.Message, "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
+
+        private void StopButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (capture != null)
+                {
+                    capture.Pause();
+                    capture.Dispose();
+                    capture = null;
+                    pictureBox1.Image.Dispose();
+                    pictureBox1.Image = null;
+                    selectedCameraid = 0;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void toolStripComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            selectedCameraid = toolStripComboBox1.SelectedIndex;
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        Point LastPoint;
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                this.Left += e.X - LastPoint.X;
+                this.Top += e.Y - LastPoint.Y;
+            }
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            LastPoint = new Point(e.X, e.Y);
+        }
+
+        //private void RecognizeButton_Click(object sender, EventArgs e)
+        //{
+        //    try
+        //    {
+        //        DialogResult res = openFileDialog1.ShowDialog();
+        //        if (res == DialogResult.OK)
+        //        {
+        //            string path = openFileDialog1.FileName;
+        //            pictureBox1.Image = Image.FromFile(path);
+        //            Bitmap bitmap = new Bitmap(pictureBox1.Image);
+        //            Image<Bgr, byte> grayImage = new Image<Bgr, byte>(bitmap);
+        //            Rectangle[] faces = classifierface.DetectMultiScale(grayImage, 1.01,2);
+        //            foreach (Rectangle face in faces)
+        //            {
+        //                using (Graphics graphics = Graphics.FromImage(bitmap))
+        //                {
+        //                    using (Pen pen = new Pen(Color.Red, 3))
+        //                    {
+        //                        graphics.DrawRectangle(pen, face);
+        //                    }
+        //                }
+        //            }
+        //            Rectangle[] eyes = classifiereye.DetectMultiScale(grayImage,1.08,13);
+        //            foreach (Rectangle eye in eyes)
+        //            {
+        //                using (Graphics graphics = Graphics.FromImage(bitmap))
+        //                {
+        //                    using (Pen pen = new Pen(Color.Yellow, 3))
+        //                    {
+        //                        graphics.DrawRectangle(pen, eye);
+        //                    }
+        //                }
+        //            }
+        //            pictureBox1.Image = bitmap;
+        //        }   
+        //        else
+        //        {
+        //            MessageBox.Show("Изображение не выбрано", "Ошибка");
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        MessageBox.Show(ex.Message, "Ошибка");
+        //    }
+        //}
     }
 }
